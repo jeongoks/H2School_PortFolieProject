0eab2e4e426447073700331f4ccfedb52b345811
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-02-21 20:08:01 +0100

    Made it possible to Route to a new page upon clicking the Submit button.

---
+++ H2School_PortFolieProject/Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H2School_PortFolieProject
+{
+    public class Guest
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        [Phone]
+        public string Mobile { get; set; }
+        public string TextBox { get; set; }
+    }
+}
